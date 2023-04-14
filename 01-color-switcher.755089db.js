9d2e44bae7a8ba31dfd43c501af21bf3d68fd8fa
KatarzynaCependa <116761001+KatarzynaCependa@users.noreply.github.com>
2023-04-14 17:44:27 +0000

    Deploying to gh-pages from @ KatarzynaCependa/goit-js-hw-09@d0d4a3555f457ff8527396d0ee340f2be5a56383 🚀

---
+++ 01-color-switcher.755089db.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");let r=null;t.addEventListener("click",(()=>{r=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.setAttribute("disabled",""),e.disabled=!1})),e.addEventListener("click",(()=>{t.disabled=!1,clearInterval(r),e.setAttribute("disabled","")}));
+//# sourceMappingURL=01-color-switcher.755089db.js.map
