d0d4a3555f457ff8527396d0ee340f2be5a56383
Katarzyna Cependa <kasia.cependa@gmail.com>
2023-04-14 19:43:53 +0200

    Adding task no. 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerId = null;
+
+const onStartClick = () => {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', '');
+  stopBtn.disabled = false;
+};
+
+const onStopClick = () => {
+  startBtn.disabled = false;
+  clearInterval(timerId);
+  stopBtn.setAttribute('disabled', '');
+};
+
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
