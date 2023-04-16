0237dabee9e9d42a0395e99f4c4eae5885ce2c0e
Katarzyna Cependa <kasia.cependa@gmail.com>
2023-04-16 17:21:12 +0200

    Adding script to task no.3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,25 +3,42 @@ import Notiflix from 'notiflix';
 const firstDelayInput = document.querySelector('input[name=delay]');
 const delayStepInput = document.querySelector('input[name=step]');
 const amountInput = document.querySelector('input[name=amount]');
-const btnEl = document.querySelector('button[type=submit]');
+const formEl = document.querySelector('.form');
 
-const createPromise = (position, delay) => {
+formEl.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const delay = parseInt(firstDelayInput.value);
+  const step = parseInt(delayStepInput.value);
+  const amount = parseInt(amountInput.value);
+
+  for (let i = 0; i < amount; i++) {
+    const position = i + 1;
+    const promiseDelay = delay + i * step;
+
+    createPromise(position, promiseDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+});
+
+function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    } else {
-      reject(`❌ Rejected promise ${position} in ${delay}ms`);
-    }
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
-};
-
-createPromise()
-  .then(({ position, delay }) => {
-    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
-
-btnEl.addEventListener('click', createPromise());
+}
